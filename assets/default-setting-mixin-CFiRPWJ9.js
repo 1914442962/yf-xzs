29e50af10ebba2623b94aa7bb2c730d1c5b7765b
1914442962 <1914442962@qq.com>
2026-02-28 10:50:07 +0800

    初始化

--- assets/default-setting-mixin-CFiRPWJ9.js
+++
@@ -1,1 +0,0 @@
-var a="运方业务智链驱动平台",e="0.1.6-beta-260203.01";const t={data:()=>({version:"v"+e,projectName:a}),methods:{}};export{t as D};
--- assets/default-setting-mixin-legacy-yEMrJyfl.js
+++
@@ -1,1 +0,0 @@
-System.register([],function(e,t){"use strict";return{execute:function(){var t="运方业务智链驱动平台",r="0.1.6-beta-260203.01";e("D",{data:()=>({version:"v"+r,projectName:t}),methods:{}})}}});
